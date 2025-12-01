08a227943958d4405d35d4b20b667d138214d23a
Junior620 <juniorlamenace@hotmail.com>
2025-12-01 10:05:13 +0100

    Add Session and Notification to models imports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,5 +3,7 @@ from .planter import Planter
 from .delivery import Delivery
 from .chef_planter import ChefPlanteur
 from .collecte import Collecte
+from .notification import Notification
+from .session import Session
 
-__all__ = ["User", "Planter", "Delivery", "ChefPlanteur", "Collecte"]
+__all__ = ["User", "Planter", "Delivery", "ChefPlanteur", "Collecte", "Notification", "Session"]
